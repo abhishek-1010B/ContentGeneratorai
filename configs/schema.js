b286bcd72af95175feb136611a6418c3c910c269
adiaparmar <adiaparmar@gmail.com>
2024-11-17 15:39:12 +0530

    Save Input to Database

--- configs/schema.js
+++ configs/schema.js
@@ -1,8 +1,18 @@
-import { pgTable, boolean, serial, varchar } from "drizzle-orm/pg-core";
+import { pgTable, boolean, serial, varchar, json } from "drizzle-orm/pg-core";
 
 export const USER_TABLE = pgTable("users", {
   id: serial().primaryKey(),
   userName: varchar().notNull(),
   email: varchar().notNull(),
   isMember: boolean().default(false),
+});
+
+export const STUDY_MATERIAL_TABLE = pgTable("studyMaterial", {
+  id: serial().primaryKey(),
+  courseId: varchar().notNull(),
+  courseType: varchar().notNull(),
+  topic: varchar().notNull(),
+  difficultyLevel: varchar().default("Easy"),
+  courseLayout: json(),
+  createdBy: varchar().notNull(),
 });
