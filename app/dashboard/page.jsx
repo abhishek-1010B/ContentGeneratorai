66e457b1e2e567134e8b3d6c4fdf71282b47d90f
adiaparmar <adiaparmar@gmail.com>
2024-12-29 16:50:09 +0530

    Deployement Issues

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,21 @@
+// /dashboard/page.js
+"use client";
+
 import React from "react";
-import WelcomeBanner from "./_components/WelcomeBanner";
-import CourseList from "./_components/CourseList";
-import ButtonsPart from "./_components/ButtonsPart";
+import dynamic from "next/dynamic";
+
+// Import components with dynamic loading for those that need window
+const WelcomeBanner = dynamic(() => import("./_components/WelcomeBanner"), {
+  ssr: false,
+});
+
+const ButtonsPart = dynamic(() => import("./_components/ButtonsPart"), {
+  ssr: false,
+});
+
+const CourseList = dynamic(() => import("./_components/CourseList"), {
+  ssr: false,
+});
 
 function Dashboard() {
   return (
